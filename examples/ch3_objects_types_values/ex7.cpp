fb23cdb94360bdb60a578e647ce6216b5388514c
Josh Deaton <jdeaton@users.noreply.github.com>
2018-10-27 13:43:08 -0400

    Ch3, Ex 7: Simple string comparison.

---
+++ examples/ch3_objects_types_values/ex7.cpp
@@ -0,0 +1,23 @@
+// String comparison.
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Please enter two names: ";
+    string first;
+    string second;
+    cin >> first >> second;
+
+    if(first == second) {
+        cout << "That's the same name twice! \n";
+    }
+    if (first < second) {
+        cout << first << " is alphabetically before " << second << "\n";
+    }
+    if (first > second) {
+        cout << first << " is alphabetically after " << second << "\n";
+    }
+
+    return 0;
+}
