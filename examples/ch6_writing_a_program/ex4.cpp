d1375c85ee4dc2d11ad31ce579d190c91bfa2811
Josh Deaton <jdeaton@users.noreply.github.com>
2018-10-28 13:03:42 -0400

    Ch6, Ex4: Implementation of Token_stream.
    Program currently not functioning properly. Development continues into Chapter 7.

--- examples/ch6_writing_a_program/ex4.cpp
+++ examples/ch6_writing_a_program/ex4.cpp
@@ -8,13 +8,57 @@ public:
     double value;
 };
 
-Token get_token();
+class Token_stream {
+public:
+    Token_stream();
+    Token get();
+    void putback(Token t);
+private:
+    bool full {false};
+    Token buffer;
+};
+
+void Token_stream::putback(Token t) {
+    if (full) { error("Cannot putback() into a full buffer."); }
+    buffer = t;
+    full = true;
+}
+
+Token Token_stream::get() {
+    if (full) { // return token in buffer if present
+        full = false;
+        return buffer;
+    }
+
+    char ch;
+    cin >> ch;
+
+    switch (ch) {
+        case ';': // print
+        case 'q': // quit
+        case '(': case ')': case '+': case '-': case '*': case '/':
+            return Token {ch};
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        {
+            cin.putback(ch); // put digit back into input stream
+            double val;
+            cin >> val;
+            return Token {'8', val}; // '8' represents number
+        }
+        default:
+            error("Bad token.");
+    }
+}
+
+
+Token get_token() {
+    return Token();
+}
 double expression();
 double term();
 double primary();
-
-Token get_token() {
-}
 
 double expression() {
     double left = term();
@@ -65,19 +109,5 @@ double primary() {
 
 int main()
 {
-    try {
-        while(cin) {
-            cout << expression() << "\n";
-        }
-    }
-    catch(exception& e) {
-        cerr << e.what() << "\n";
-        return 1;
-    }
-    catch(...) {
-        cerr << "Exception. \n";
-        return 2;
-    }
-
     return 0;
 }
