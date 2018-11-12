c7c4e6fe002cba1efdb659bc100bfe90c2528be2
Josh Deaton <jdeaton@users.noreply.github.com>
2018-11-12 07:55:12 -0500

    Ch3, Ex3: Valid/invalid names to see how compiler reacts.

---
+++ exercises/ch3_objects_types_values/exercise_03.cpp
@@ -0,0 +1,17 @@
+/* Ch 3. - Exercise 3
+ *
+ * Write a program that doesn't do anything, but declares a
+ * number of variables with legal and illegal names (such as
+ * int double = 0;), so that you can see how the compiler
+ * reacts.
+ */
+
+int main()
+{
+    int legal_name = 0;
+    int legalName = 0;
+
+    //int double = 0;
+    //int 2illegal_name = 0;
+    //int illegal-name = 0;
+}
